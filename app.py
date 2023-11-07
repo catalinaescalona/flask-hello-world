7ba29b40b77df4b4435aae32fa22794e365d7b61
Catalina Escalona <catalinaescalona@gmail.com>
2023-11-07 13:43:37 +0000

    db_test route commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://catalina_lab_10_db_user:yPo6HWW8sTwX2D7cBG2DcqXyyDLVGg1l@dpg-cl53dnil7jac73cb8vo0-a/catalina_lab_10_db")
+    conn.close()
+    return "Database Connection Successful"
